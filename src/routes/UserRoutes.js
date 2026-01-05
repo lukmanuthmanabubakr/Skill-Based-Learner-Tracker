4e1c47189d400d20e9134cc8de76082da797e125
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2026-01-05 15:14:27 +0100

    Fix auth middleware and enable protected user route

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { registerUser } from "../controllers/userControllers.js";
+import {
+  getUser,
+  loginUser,
+  registerUser,
+} from "../controllers/userControllers.js";
+import { protect } from "../middleware/authToken.js";
 
 const userRoutes = express.Router();
 
-userRoutes.post('/register', registerUser)
+userRoutes.post("/register", registerUser);
+userRoutes.post("/login", loginUser);
+userRoutes.get("/users/me", protect, getUser);
 
-export default userRoutes
+export default userRoutes;
