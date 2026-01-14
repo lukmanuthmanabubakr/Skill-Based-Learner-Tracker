519298db02142fc1b6f21b25ab0a4203e8f3edcd
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2026-01-14 19:30:25 +0100

    update

--- src/routes/EvidenceRoute.js
+++ src/routes/EvidenceRoute.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { protect } from "../middleware/authToken.js";
-import { getEvidenceForSkill, deleteEvidence, createEvidence } from "../controllers/evidenceControllers.js";
+import {
+  getEvidenceForSkill,
+  deleteEvidence,
+  createEvidenceForSkill,
+  createEvidenceForPractice,
+} from "../controllers/evidenceControllers.js";
 
 const evidenceRouter = express.Router();
 
-evidenceRouter.post("/", protect, createEvidence);
+evidenceRouter.post("/skills/:skillId/evidence", protect, createEvidenceForSkill);
+evidenceRouter.post(
+  "/practice-logs/:practiceLogId/evidence",
+  protect,
+  createEvidenceForPractice
+);
 evidenceRouter.get("/:skillId", protect, getEvidenceForSkill);
 evidenceRouter.delete("/:evidenceId", protect, deleteEvidence);
 
